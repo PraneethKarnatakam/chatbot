2ca428c8d54ddeb4ff4fe10da0b42b079d40b529
PraneethKarnatakam <106651164+PraneethKarnatakam@users.noreply.github.com>
2024-07-25 15:39:42 +0530

    json binding and features call

--- src/main/java/com/zoomcar/ChatBot/controller/growthbook/GrowthBookRefreshController.java
+++ src/main/java/com/zoomcar/ChatBot/controller/growthbook/GrowthBookRefreshController.java
@@ -1,13 +1,18 @@
 package com.zoomcar.ChatBot.controller.growthbook;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.zoomcar.ChatBot.service.CacheService;
+import com.zoomcar.ChatBot.service.GrowthBookManager;
 import com.zoomcar.ChatBot.service.QdrantService;
 import org.redisson.api.RedissonClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import retrofit2.http.POST;
 
 @RestController
 @RequestMapping("/growthbook")
@@ -16,10 +21,22 @@ public class GrowthBookRefreshController {
     @Autowired
     private CacheService cacheService;
 
+    @Autowired
+    private GrowthBookManager growthBookManager;
+
     @GetMapping("/get")
     public String model(@RequestParam(value = "message", defaultValue = "Hello") String key) {
         var result = cacheService.get("hello");
         return result;
+    }
+
+    @PostMapping("/repopulate")
+    public void populate () throws Exception {
+        String features = growthBookManager.fetchFeatures();
+        cacheService.put(features,);
+        if(features != null) {
+        }
+
     }
 
 
--- src/main/java/com/zoomcar/ChatBot/service/GrowthBookManager.java
+++ src/main/java/com/zoomcar/ChatBot/service/GrowthBookManager.java
@@ -1,6 +1,8 @@
 package com.zoomcar.ChatBot.service;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.gson.JsonElement;
 import com.zoomcar.ChatBot.configuration.GrowthBookProperties;
 import growthbook.sdk.java.GBContext;
 import growthbook.sdk.java.GrowthBook;
@@ -8,7 +10,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,7 +25,46 @@ public class GrowthBookManager {
     @Autowired
     private GrowthBookProperties growthBookProperties;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     private final String fetchFeaturesPath = "/api/features/";
+
+    public String fetchFeatures()  throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("project", growthBookProperties.getProjectKey());
+        String response = null;
+        try {
+            response = apiCallFeatures(growthBookProperties.getHostUrl(), fetchFeaturesPath,growthBookProperties,params,growthBookProperties.getRequestTimeout());
+            //String response = HttpUtils.executeHttpRequest(HttpMethod.GET, hostUrl + fetchFeaturesPath + growthBookProperties.getApiKey(), null, null, params, null,requestTimeOut);
+        }
+        catch (Exception e) {
+           throw e;
+        }
+        return response;
+    }
+
+    public String apiCallFeatures(String hostUrl, String fetchFeaturesPath, GrowthBookProperties growthBookProperties, Map<String, String> params, int requestTimeOut) {
+        String apiKey = growthBookProperties.getApiKey();
+        String url = hostUrl + fetchFeaturesPath + apiKey;
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
+        if (params != null) {
+            params.forEach(uriBuilder::queryParam);
+        }
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                uriBuilder.toUriString(),
+                HttpMethod.GET,
+                null,
+                String.class
+        );
+        return responseEntity.getBody();
+    }
+
+
+
+
+
+
 
 
 
